253e6cf025d172bb29daee69a1c609b6da98c1fb
Guillaume Bielli <guillaume.bielli@gmail.com>
2026-03-13 15:21:59 +0100

    metrics

--- app/layout.tsx
+++ app/layout.tsx
@@ -84,7 +84,7 @@ export default function RootLayout({
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://tss.familytribe.fr/yzsnjsswoaaea9i.js?awl='+i.replace(/^GTM-/, '')+dl;f.parentNode.insertBefore(j,f);
+'/metrics/?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-MSRMPCVF');`,
           }}
         />
